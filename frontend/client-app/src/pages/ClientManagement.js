7530b3a14bd41123a0f0b53d38d81b5fe613cb6e
Johané Badenhorst <577401@student.belgiumcampus.ac.za>
2024-11-03 11:27:39 +0200

    Client Management page updated

--- frontend/client-app/src/pages/ClientManagement.js
+++ frontend/client-app/src/pages/ClientManagement.js
@@ -8,6 +8,7 @@ function ClientManagement() {
   };
 
   return (
+  <div>
     <div className="App">
       <header className='nav'>
         <button onClick={goToDashboard} className="dashboard-button">
@@ -22,6 +23,49 @@ function ClientManagement() {
         <h1>Client Management</h1>
       </header> 
     </div>
+
+    <label for='searchID' class='searchLabel'>ID:</label>
+    <input type='text' name='searchID' class='searchID' placeholder='Enter ID to search client'></input>
+    <button type='submit' class='searchButton'>Search ID</button><br/>
+
+    <h4>Name</h4><h4>Surname</h4><br/>
+    <h4>Email: xxx</h4><h4>Phone number: xxx</h4><h4>Address: xxx</h4><br/>
+    <div className='activeContract'>
+    <h3>Active Contracts</h3>
+    <table>
+      <tr>
+        <th>Request ID</th>
+        <th>Job ID</th>
+        <th>Completion Date</th>
+      </tr>
+      <tr>
+        <td>NP026</td>
+        <td>A7</td>
+        <td>2025/01/01</td>
+      </tr>
+    </table>
+    </div>
+
+    <div className='contractHistory'>
+    <h3>Contract History</h3>
+    <table>
+      <tr>
+        <th>Request ID</th>
+        <th>Job ID</th>
+        <th>Description</th>
+        <th>Completion date</th>
+        <th>Average service rating</th>
+      </tr>
+      <tr>
+        <td>NP020</td>
+        <td>A1</td>
+        <td>Repair cracked windshield on vehicle.</td>
+        <td>2020/10/20</td>
+        <td>4.5</td>
+      </tr>
+    </table>
+    </div>
+  </div>
   );
 }
 
